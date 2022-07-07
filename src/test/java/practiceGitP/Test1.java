ff2a105d7bc723e4fa4fa89f61a50a9ae14686e1
byamini <baratamyamini222@gmail.com>
2022-07-07 19:26:55 +0530

    Updated existing class and created new class

--- src/test/java/practiceGitP/Test1.java
+++ src/test/java/practiceGitP/Test1.java
@@ -1,6 +1,6 @@
 package practiceGitP;
 
-public class Test1 {
+public class Test1 extends Test2 {
 	
 	 String College;
 
@@ -8,10 +8,16 @@ public class Test1 {
 		
 		System.out.println("Practicing GITHUB");
 		
-		//added new lines of code to commit changes to local reposiroty
+		//added new lines of code to commit changes to local repository
 		
-		
+		//Trying to add new line in existing and creating new file to commit the changes to remote repository
 
+		
+	}
+	
+	public void method1()
+	{
+		System.out.println("from parent class");
 	}
 
 }
---
+++ src/test/java/practiceGitP/Test2.java
@@ -0,0 +1,10 @@
+package practiceGitP;
+
+public class Test2 {
+	
+	public void method1()
+	{
+		System.out.println("from child class");
+	}
+
+}

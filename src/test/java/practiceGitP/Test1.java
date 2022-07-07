21e76cf67f0aaed00de256438683cdcf3dafe009
byamini <baratamyamini222@gmail.com>
2022-07-07 17:28:45 +0530

    added new changes

--- src/test/java/practiceGitP/Test1.java
+++ src/test/java/practiceGitP/Test1.java
@@ -1,10 +1,16 @@
 package practiceGitP;
 
 public class Test1 {
+	
+	 String College;
 
 	public static void main(String[] args) {
 		
 		System.out.println("Practicing GITHUB");
+		
+		//added new lines of code to commit changes to local reposiroty
+		
+		
 
 	}
 
